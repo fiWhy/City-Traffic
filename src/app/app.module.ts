affec8871fabbce55272284472b4ed6bd68565c7
Denis <fiwhy@mail.ru>
2017-05-24 16:23:26 +0300

    Local components for app
    #Create local components structure for app
    #Create top bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import * as angular from "angular";
 
 import { App } from "./app.component";
+import { TopBar } from "./components";
 
 import { materialConfig } from "./config";
 
@@ -11,6 +12,7 @@ angular.module("app", [
     "ngMdIcons",
 ])
     .config(materialConfig)
+    .component(TopBar.selector, TopBar)
     .component(App.selector, App);
 
 angular.bootstrap(document.getElementById("app"), ["app"]);
