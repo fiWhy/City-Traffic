18fe696a33914a3cbec94607adb1d4abfa41ed62
Денис <fiwhy@mail.ru>
2017-06-06 23:18:45 +0300

    Connect dashboard and topbar
    #Added menu config to dashboard
    #Added nav-bar to top-bar component
    #Removed overlogic from traffic-map

--- src/app/states/dashboard/components/traffic-map/traffic-map.component.ts
+++ src/app/states/dashboard/components/traffic-map/traffic-map.component.ts
@@ -3,45 +3,12 @@ import "./traffic-map.scss";
 export class TrafficMapController {
     static $inject = ["NgMap", "$scope"];
     private zoom: number;
-    private center: google.maps.LatLng;
+    private center: any;
     private defaultZoom: number = 10;
     constructor(private NgMap,
         private $scope: ng.IScope, ) {
-        this.initMap();
-        this.setWatchers();
     }
 
-    private initMap() {
-        this.NgMap.getMap().then((map) => {
-        });
-    }
-
-    private setWatchers() {
-        this.$scope.$watch("TrafficMap.center", (val: google.maps.LatLng) => this.backToCenter(val));
-        this.$scope.$watch("TrafficMap.zoom", (val: number) => this.backToZoom(val));
-    }
-
-    private setCenter(pos: google.maps.LatLng): void {
-        this.NgMap.getMap().then((map) => {
-            this.backToCenter(pos);
-            this.backToZoom(this.zoom || this.defaultZoom);
-        });
-    }
-
-    private backToCenter(center: google.maps.LatLng) {
-        if (center) {
-            this.NgMap.getMap().then((map) => {
-                google.maps.event.trigger(map, "resize");
-                map.setCenter(center);
-            });
-        }
-    }
-
-    private backToZoom(zoom: number) {
-        this.NgMap.getMap().then((map) => {
-            map.setZoom(zoom || this.defaultZoom);
-        });
-    }
 }
 
 export const TrafficMap = {
--- src/app/states/dashboard/dashboard.module.ts
+++ src/app/states/dashboard/dashboard.module.ts
@@ -4,7 +4,9 @@ import { Dashboard } from "./dashboard.component";
 import { TrafficMap } from "./components";
 
 import { routes } from "./dashboard.route";
+import { menu } from "./config/menu.config";
 
 export const DashboardModule: ng.IModule = angular.module("app.states.dashboard", [])
     .config(routes)
+    .config(menu)
     .component(TrafficMap.selector, TrafficMap);
