9ecf7ee6377755f06cc1f8702c43c73a84b985aa
Денис <fiwhy@mail.ru>
2017-06-06 23:42:00 +0300

    First menu item
    #Added menu config to dashboard

--- src/app/states/dashboard/config/menu.config.ts
+++ src/app/states/dashboard/config/menu.config.ts
@@ -1,4 +1,11 @@
 import { NavBarServiceProvider } from "../../../shared/components/nav-bar";
 
 export const menu = (NavBarServiceProvider: NavBarServiceProvider) => {
+    NavBarServiceProvider.addMenuItem({
+        sref: "app.dashboard",
+        title: "Dashboard",
+        name: "dashboard",
+        active: true,
+        order: 1,
+    });
 };
