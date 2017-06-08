7c230150e81b6c7e528f02503304ab828de73c95
Denis <fiwhy@mail.ru>
2017-06-08 17:27:11 +0300

    GeoService
    #Added core layer
    #Added core services layer
    #Added geo service with dependencies
    #Added logic to get current position

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,25 @@ import { materialConfig, routesConfig } from "./config";
 
 import { StatesModule } from "./states";
 import { SharedModule } from "./shared";
+import { CoreModule } from "./core";
 
 import { routes } from "./app.route";
+
+import { AppService } from "./app.service";
 
 angular.module("app", [
     "ui.router",
     "ngMaterial",
     "ngMdIcons",
     "ngMap",
+    CoreModule.name,
     SharedModule.name,
     StatesModule.name,
 ])
     .config(routes)
     .config(materialConfig)
     .config(routesConfig)
+    .service("AppService", AppService)
     .component(TopBar.selector, TopBar)
     .component(SideNav.selector, SideNav);
 
