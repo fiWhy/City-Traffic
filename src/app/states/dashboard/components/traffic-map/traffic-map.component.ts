e5aebae29fa3e2230f23602aafc8c9c67f0afb0e
Denis <fiwhy@mail.ru>
2017-07-13 12:59:02 +0300

    Directions
    # Added direction select functionality
    # Added traffic map direction show functionality

--- src/app/states/dashboard/components/traffic-map/traffic-map.component.ts
+++ src/app/states/dashboard/components/traffic-map/traffic-map.component.ts
@@ -1,12 +1,58 @@
 import "./traffic-map.scss";
+import { Contribution } from "../../../../core/entities";
 
 export class TrafficMapController {
     static $inject = ["NgMap", "$scope"];
     private zoom: number;
     private center: any;
     private defaultZoom: number = 10;
+    private preparedDirection: any;
     constructor(private NgMap,
         private $scope: ng.IScope, ) {
+        this.setWatchers();
+    }
+
+    private setWatchers() {
+        this.$scope.$watch("TrafficMap.center", (val: google.maps.LatLng) => this.backToCenter(val));
+        this.$scope.$watch("TrafficMap.direction", (val: Contribution) => {
+            if (val) {
+                this.prepareDirection(val);
+            } else {
+                return;
+            }
+        });
+        this.$scope.$watch("TrafficMap.zoom", (val: number) => this.backToZoom(val));
+    }
+
+
+    private setCenter(pos: google.maps.LatLng): void {
+        this.NgMap.getMap().then((map) => {
+            this.backToCenter(pos);
+            this.backToZoom(this.zoom || this.defaultZoom);
+        });
+    }
+
+    private backToCenter(center: google.maps.LatLng) {
+        if (center) {
+            this.NgMap.getMap().then((map) => {
+                google.maps.event.trigger(map, "resize");
+                map.setCenter(center);
+            });
+        }
+    }
+
+    private backToZoom(zoom: number) {
+        this.NgMap.getMap().then((map) => {
+            map.setZoom(zoom || this.defaultZoom);
+        });
+    }
+
+    private prepareDirection(direction: Contribution) {
+        this.preparedDirection = {
+            origin: `${direction.startPoint.lat()}, ${direction.startPoint.lng()}`,
+            destination: `${direction.endPoint.lat()}, ${direction.endPoint.lng()}`,
+            waypoints: direction.additional
+        };
     }
 
 }
@@ -19,5 +65,6 @@ export const TrafficMap = {
     bindings: {
         zoom: "<",
         center: "<",
+        direction: "<",
     },
 };
--- src/app/states/dashboard/dashboard.component.ts
+++ src/app/states/dashboard/dashboard.component.ts
@@ -1,8 +1,14 @@
 import { IAuthProvider } from "../../core/providers";
+import { Contribution } from "../../core/entities";
 
 export class DashboardController {
     static $inject = ["AuthProvider"];
+    private selectedDirection: Contribution;
     constructor(private AuthProvider: IAuthProvider) { }
+
+    private directionSelected(direction: Contribution) {
+        this.selectedDirection = new Contribution(direction);
+    }
 }
 export const Dashboard = {
     selector: "dashboard",
